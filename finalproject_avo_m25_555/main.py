f8100331158b3f2e466dc572ae1fe812a7b8cc80
An <6878811@mail.ru>
2026-01-06 14:40:04 +0300

    feat: main.py filled with data

---
+++ finalproject_avo_m25_555/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Entry point проекта."""
+
+import valutatrade_hub.cli.interface as cli
+
+
+def main():
+    """Запуск CLI."""
+    cli.main()
+
+
+if __name__ == "__main__":
+    main()
